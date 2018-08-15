ddea1ed9183537667636fd2849eeb181c07c2701
John Glassmyer <john@johnglassmyer.net>
2018-08-15 00:13:00 -0700

    UltimaPatcher: helpful message for invalid segmented address

--- UltimaPatcher/src/main/java/net/johnglassmyer/ultimapatcher/SegmentedAddress.java
+++ UltimaPatcher/src/main/java/net/johnglassmyer/ultimapatcher/SegmentedAddress.java
@@ -3,15 +3,18 @@ package net.johnglassmyer.ultimapatcher;
 import static com.google.common.base.Verify.verify;
 
 class SegmentAndOffset {
+	private static final String INVALID_ADDRESS_MESSAGE_TEMPLATE =
+			"\"%s\" is not a valid segmented address; should be segmentIndex:offset";
+
 	static SegmentAndOffset fromString(String string) {
 		String[] segments = string.split(":");
-		verify(segments.length == 2);
+		verify(segments.length == 2, INVALID_ADDRESS_MESSAGE_TEMPLATE, string);
 
 		int segmentIndex = Integer.valueOf(segments[0]);
-		verify(segmentIndex >= 0);
+		verify(segmentIndex >= 0, INVALID_ADDRESS_MESSAGE_TEMPLATE, string);
 
 		int offset = Integer.decode(segments[1]);
-		verify(offset >= 0);
+		verify(offset >= 0, INVALID_ADDRESS_MESSAGE_TEMPLATE, string);
 
 		return new SegmentAndOffset(segmentIndex, offset);
 	}
